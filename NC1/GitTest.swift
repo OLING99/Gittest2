992a14ec9b6d04abff031ed3df5b64c96e48dd0e
OLING <128208686+OLING99@users.noreply.github.com>
2023-07-14 15:36:00 +0900

    [feat]새로운 git연습 파일 추가

---
+++ NC1/GitTest.swift
@@ -0,0 +1,20 @@
+//
+//  GitTest.swift
+//  NC1
+//
+//  Created by OLING on 2023/07/14.
+//
+
+import SwiftUI
+
+struct GitTest: View {
+    var body: some View {
+        Text("올링")
+    }
+}
+
+struct GitTest_Previews: PreviewProvider {
+    static var previews: some View {
+        GitTest()
+    }
+}
